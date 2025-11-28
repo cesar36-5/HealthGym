9f203f1eeae327167dab2bb89b7107e0dbb00525
yamineyra <yami05@neyracontreras@gmail.com>
2025-11-28 01:14:09 -0500

    Yamilet: Agregue EvaluaciónNutricional

--- CapaDatos/DatEvaluacionNutricional.cs
+++ CapaDatos/DatEvaluacionNutricional.cs
@@ -60,7 +60,7 @@ namespace CapaDatos
                 return count > 0;
             }
         }
-        //Buscar Miembro por DNI
+        //Buscar Miembro 
         public EntEvaluacionNutricional? BuscarPorDNI(string dni)
         {
             if (string.IsNullOrWhiteSpace(dni))
